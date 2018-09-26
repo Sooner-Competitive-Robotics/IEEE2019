532151d65609e527f44f6f2696f11201601b6c82
josephchang98 <32658356+josephchang98@users.noreply.github.com>
2018-09-25 20:22:04 -0500

    should be correct

--- FowardsAndBackwards/FowardsAndBackwards.ino
+++ FowardsAndBackwards/FowardsAndBackwards.ino
@@ -1,25 +1,28 @@
 #include <RobotLib.h>
-#include <IEEErobot2019.h>
-
+#include "StepperMotorDrivetrain.h"
 bool turnDone = false;
+
+StepperMotorDrivetrain drivetrain;
+
 
 void setup()
 {
   //Initialize motors and basic drivetrain functions
-  drivetrain.initLeft(LEFT_IN_1, LEFT_IN_2, LEFT_IN_3, LEFT_IN_4);
-  drivetrain.initRight(RIGHT_IN_1, RIGHT_IN_2, RIGHT_IN_3, RIGHT_IN_4);
-  drivetrain.setRPM(DRIVE_RPM);
+  //TODO: Specify pins, right now they're based on last year
+  //drivetrain.initLeft(16, 15, 25, 24);
+  //drivetrain.initRight(10, 11, 8, 7);
+  drivetrain.setRPM(25);
 }
 
 void loop()
 {
-  drivetrain.setRPM(DRIVE_RPM);
+  drivetrain.setRPM(25);
   drivetrain.step(drivetrain.convertInchesToSteps(12), drivetrain.convertInchesToSteps(12));
-  commandTransition();
+  delay(500);
 
-  drivetrain.setRPM(DRIVE_RPM);
+  drivetrain.setRPM(25);
   drivetrain.step(drivetrain.convertInchesToSteps(-12), drivetrain.convertInchesToSteps(-12));
-  commandTransition();
+  delay(500);
 
   while(true){};
 }
