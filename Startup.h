179868e9e63902e5192ac6190b7576d95d85ca25
Hambonaire <32001455+Hambonaire@users.noreply.github.com>
2019-01-22 17:50:15 -0600

    Startup back to 4 motors on one Nano

--- Startup.h
+++ Startup.h
@@ -1,12 +1,14 @@
 #ifndef STARTUP_H
 #define STARTUP_H
 
-//StepperMotorDrivetrain drivetrain;
-SteeperHalfDrivetrain drivetrain;
+StepperMotorDrivetrain drivetrain;
+//SteeperHalfDrivetrain drivetrain;
 
 void robotSetup() {
-	drivetrain.initFront(FRONT_LEFT_MOT_PIN1, FRONT_LEFT_MOT_PIN2, FRONT_LEFT_MOT_PIN3, FRONT_LEFT_MOT_PIN4);
-	drivetrain.initBack(FRONT_RIGHT_MOT_PIN1, FRONT_RIGHT_MOT_PIN2, FRONT_RIGHT_MOT_PIN3, FRONT_RIGHT_MOT_PIN4);
+	drivetrain.initFrontLeft(FRONT_LEFT_MOT_PIN1, FRONT_LEFT_MOT_PIN2, FRONT_LEFT_MOT_PIN3, FRONT_LEFT_MOT_PIN4);
+	drivetrain.initBackLeft(BACK_LEFT_MOT_PIN1, BACK_LEFT_MOT_PIN2, BACK_LEFT_MOT_PIN3, BACK_LEFT_MOT_PIN4);
+	drivetrain.initFrontRight(FRONT_RIGHT_MOT_PIN1, FRONT_RIGHT_MOT_PIN2, FRONT_RIGHT_MOT_PIN3, FRONT_RIGHT_MOT_PIN4);
+	drivetrain.initBackRight(BACK_RIGHT_MOT_PIN1, BACK_RIGHT_MOT_PIN2, BACK_RIGHT_MOT_PIN3, BACK_RIGHT_MOT_PIN4);
 	drivetrain.setRPM(25);
 }
 
