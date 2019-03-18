27bbb68a3f7fd8ab695ccc3ae7f178155cbb9524
Hambonaire <32001455+Hambonaire@users.noreply.github.com>
2019-03-18 04:02:05 -0500

    ...Startup

--- Startup.h
+++ Startup.h
@@ -99,6 +99,7 @@ void robotSetup()
 	arm.begin(fistServo, wristServo, rackAndPinion, lowSwitch, highSwitch);
 	
 	Serial.println("Initialize MPU6050");
+	mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G);
 	/*while(!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G))
 	{
 		Serial.println("Could not find a valid MPU6050 sensor, check wiring!");
